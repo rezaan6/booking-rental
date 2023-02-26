0523efff482f8e9fa97ae1fff645b14a825e1ae2
rezaan6 <rezaan6@gmail.com>
2023-02-26 21:01:32 +0530

    update Header.js

--- components/Header.js
+++ components/Header.js
@@ -16,6 +16,7 @@ const Header = () => {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
 
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
@@ -65,11 +66,22 @@ const Header = () => {
             rangeColors={["#FD5B61"]}
             onChange={handleSelect}
           />
-          <div className="flex items-center border-b">
+          <div className="flex items-center border-b mb-4">
             <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
           </div>
 
-          <UsersIcon className="h-5" />
+          <div className="flex">
+            <button type="">Cancel</button>
+            <button type="">Search</button>
+          </div>
         </div>
       )}
     </header>
